beb4620d138ef669b9b17e7d0e2a3991562735fe
johanvergeer <johanvergeer@gmail.com>
2015-07-13 16:23:06 +0200

    13-07-2015 16:22

---
+++ Modularity.py
@@ -0,0 +1,13 @@
+import urllib
+from urllib import urlopen
+
+def fetch_words():
+    story = urlopen('http://sixty-north.com/c/t.txt')
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+
+    for word in story_words:
+        print(word)
---
+++ words.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+""" Retrieve and print words from a URL.
+
+Usage:
+
+    python3 words.py<URL>
+"""
+
+
+import sys
+from urllib import urlopen
+
+def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The URL of a UTF-8 text document.
+
+    Returns:
+        A list of string containing the words from the document.
+    """
+    story = urlopen(url)
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf-8').split()
+        for word in line_words:
+            story_words.append(word)
+    return story_words
+
+def print_items(items):
+    """ Print items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
+    for item in items:
+        print(item)
+
+def main(url):
+    """ Print each word from a text document from a URL.
+
+    Args:
+        url:  The URL of a UTF-8 text document.
+    """
+    words = fetch_words(url)
+    print_items(words)
+
+if __name__ == '__main__':
+    main(sys.argv[1]) # The 0th argument is the module filename
